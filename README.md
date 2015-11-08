c0eb17e120d11126115c6a1c31fe79459b309d5b
Marcel van Pinxteren <marcel.van.pinxteren@gmail.com>
2015-11-08 17:33:42 +0100

    Initial commit Ember app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # visualplanner-www
 Visual Planner website
+
+## Setup
+ember install ember-cli-sass
+bower install --save bootstrap-sass
+add app.scss:
+@import 'bower_components/bootstrap-sass/assets/stylesheets/bootstrap';
+
+ember install smoke-and-mirrors
---
+++ app/mirage/fixtures/stories.js
@@ -0,0 +1,44 @@
+export default
+[
+  {id: 1,
+    title: 'Vernieuwing',
+    text: `Visual Planner stelt zich ten doel moderne UI technieken
+    te introduceren in de planningsoftware`
+  },
+  {id: 2,
+    title: 'Extra dimensie',
+    text: `In plaats van lijsten en tweedimensionale plaatjes,
+    willen we een dimensie extra bieden.`
+  },
+  {id: 3,
+    title: 'Verbanden',
+    text: `In een planning zijn er altijd veel verbanden tussen de objecten in die planning.
+    Er is werk dat toebedeeld moet worden aan de beschikbare middelen,
+    zoals vrachtwagens, chauffeurs, docenten, ruimtes. De kern van het planningsproces
+    is om al deze verbanden zo optimaal mogelijk te leggen. Overzicht houden is dan
+    al gauw niet zo makkelijk.`
+  },
+  {id: 4,
+    title: 'Beperkingen',
+    text: `Een ander belangrijke opdracht voor de planner is, om niet alleen
+    een optimale planning te maken, maar ook een die uitvoerbaar is. Die
+    uitvoerbaarheid wordt gedicteerd door allerlei beperkingen. Een vrachtwagen
+    kan maar één rit tegelijk doen, in een lokaal past maar één klas met kinderen,
+    een chauffeur mag maar 8 uur per dag werken.`
+  },
+  {id: 5,
+    title: 'Ambitie',
+    text: `Het is onze ambitie om een web-frontend te bieden, waarin alle
+    verbanden en beperkingen in DRIE dimensies inzichtelijk gemaakt worden. Ons front-end is
+    aan te sluiten op bestaande
+    planningsystemen. Hierbij gebruiken we moderne technieken als Ember,
+    ThreeJS, WebGL, Ruby on Rails, Elixir. Samen met de klant gaan we onderzoeken hoe
+    haar of zijn bestaande database te koppelen is aan onze front-end applicatie.`
+  },
+  {id: 8,
+    title: 'Contact',
+    text: `info@visualplanner.nl`,
+    ref: `mailto:info@visualplanner.nl`
+  },
+
+];
